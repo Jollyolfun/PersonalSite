879d4516239fa4fd95e040b13818897151ae7c7a
Jollyolfun <90577117+Jollyolfun@users.noreply.github.com>
2024-02-17 15:13:07 -0700

    added comments and the social media section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
-
+import linkedInLogo from './images/lill.png';
+import githubLogo from './images/githublogo.png';
 import './App.css';
 
 function MySite() {
   const [scrollPosition, setScrollPosition] = useState(0);
   const aboutMeRef = useRef(null); 
+  const socialMediaRef = useRef(null); 
 
+  //Used to decide where to scroll to when a specific option
+  //on the top of the webpage is clicked
   const handleScroll = () => {
     const position = window.pageYOffset;
     setScrollPosition(position);
@@ -18,12 +22,28 @@ function MySite() {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-
+  
+  //Determines which button was clicked and where to scroll based on that
   const clickedOption = (type) => {
     console.log(type);
     if (type == 'About Me' && aboutMeRef.current) {
-      aboutMeRef.current.scrollIntoView({ behavior: 'smooth' }); // Scroll to the aboutMeContainer
+      aboutMeRef.current.scrollIntoView({ behavior: 'smooth' }); 
     }
+    if (type == 'Social Media' && socialMediaRef.current) {
+      socialMediaRef.current.scrollIntoView({behavior: 'smooth'})
+    }
+  }
+
+  //Used to open a specific webpage when one of the social media icons are clicked
+  const openWebpage = (website) => {
+    if (website == 'linkedin') {
+      window.open('https://www.linkedin.com/in/cole-perry-1712aa180/', '_blank', 'noopener,noreferrer');
+    }
+
+    if (website == 'github') {
+      window.open('https://github.com/Jollyolfun', '_blank', 'noopener,noreferrer');
+    }
+
   }
 
   return (
@@ -70,7 +90,7 @@ function MySite() {
       <div className="line-with-dots"></div>
       <div className="gap"></div>
       
-
+      {/* THIS IS THE ABOUT ME SECTION */}
       <div className="aboutMeContainer" ref={aboutMeRef}> 
         <div className="myImage"></div>
         <div className="descriptionContainer">
@@ -115,13 +135,37 @@ function MySite() {
         </div>
 
       </div>
-      
-      <div className="gap"></div>
+      {/* THIS IS THE END OF THE ABOUT ME SECTION */}
 
-      <div className="socialMediaContainer">
 
-        Hello
+      <div className="gap">
+        <div className="line-with-dots"></div>
+      </div>
 
+      {/* THIS IS THE SOCIAL MEDIA SECTION */}
+      <div className="socialMediaContainer" ref={socialMediaRef}>
+        <div className="topicHeader"> Social Media </div> 
+        <div className="socialMediaSelection">
+          {/* This is where the social media buttons are */}
+          
+          <div className="socialMediaOptionGithub" onClick={() => openWebpage('github')}> 
+            GitHub
+            <img src={githubLogo} alt="GitHub"/>
+          </div>
+
+          <div className="socialMediaOption" onClick={() => openWebpage('linkedin')}> 
+            LinkedIn 
+            <img src={linkedInLogo} alt="LinkedIn" />
+          </div>
+
+        </div>
+
+
+      </div>
+      {/* THIS IS THE END OF THE SOCIAL MEDIA SECTION */}
+
+      <div className="gap">
+        <div className="line-with-dots"></div>
       </div>
 
     </div>
